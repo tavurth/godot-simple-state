75783d634a53b2bc0a35405cb2d8aea372ec5170
Will Whitty <tavurth@gmail.com>
2023-12-11 11:13:00 +0800

    Fix 4.x import issue

--- addons/godot-simple-state/plugin.gd
+++ addons/godot-simple-state/plugin.gd
@@ -2,12 +2,16 @@
 extends EditorPlugin
 
 
+static func get_icon(name: String) -> Texture2D:
+	return load("res://addons/godot-simple-state/icon_" + name + ".png") as Texture2D
+
+
 func _enter_tree():
 	add_custom_type(
 		"StateMachine",
 		"Node",
 		preload("res://addons/godot-simple-state/states.gd"),
-		preload("res://addons/godot-simple-state/icon.png")
+		load("res://addons/godot-simple-state/icon.png")
 	)
 
 
